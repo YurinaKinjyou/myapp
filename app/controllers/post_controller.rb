d01d6813b49bc3db25367b393f1c189ea97058fa
Yurina <hydeyurina@gmail.com>
2017-12-21 23:28:20 +0900

    rails myapp

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,6 @@
 class PostController < ApplicationController
   def index
-    @post = Post.all
+    @post = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -16,6 +16,7 @@ class PostController < ApplicationController
     @post.content = params[:content]
     if @post.save
       redirect_to("/index")
+      flash[:notice] = "編集完了！"
     else
       render("/post/edit")
     end
@@ -23,18 +24,21 @@ class PostController < ApplicationController
 
   def delete
     @post = Post.find_by(id: params[:id])
-    @post.destroy
-    redirect_to("/index")
+    if @post.destroy
+      redirect_to("/index")
+      flash[:notice] = "削除完了！"
+    end
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
     @post = Post.new(content: params[:content])
     if @post.save
       redirect_to("/index")
+      flash[:notice] = "投稿完了！"
     else
       render('post/new')
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
-    # validates :content, {presence: true, length: {maximum: 140}}
-    validate :add_error
+    validates :content, {presence: true, length: {maximum: 140}}
+    # validate :add_error
  
-    def add_error
-        # nameが空のときにエラーメッセージを追加する
-        if content.blank?
-        errors[:base] << "商品名は必ず入力して下さい"
-        end
-    end
+    # def add_error
+    #     # nameが空のときにエラーメッセージを追加する
+    #     if content.blank?
+    #     errors[:base] << "商品名は必 ず入力して下さい"
+    #     end
+    # end
 end
